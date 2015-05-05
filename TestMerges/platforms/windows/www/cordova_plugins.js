3bfd25b1a930e94843fe22c9be344e513e079bba
Ashwin Desai <ashwin.desai@sap.com>
2015-05-05 11:38:57 -0400

    Add new

---
+++ TestMerges/platforms/windows/www/cordova_plugins.js
@@ -0,0 +1,39 @@
+﻿cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/src/windows/InAppBrowserProxy.js",
+        "id": "org.apache.cordova.inappbrowser.InAppBrowserProxy",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.TestPlugin/www/test.js",
+        "id": "org.apache.cordova.TestPlugin.TestPlugin",
+        "clobbers": [
+            "test.something"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.TestPlugin/src/windows/TestPluginProxy.js",
+        "id": "org.apache.cordova.TestPlugin.TestPluginProxy",
+        "merges": [
+            ""
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.inappbrowser": "0.6.0",
+    "org.apache.cordova.TestPlugin": "0.6.0"
+}
+// BOTTOM OF METADATA
+});
